65ad62afca33ea2fba0980d3d6f9e98d6c5427ec
hollowaymj <michaeljholloway@gmail.com>
2020-02-12 16:01:19 -0500

    Created terrain, ship, skybox, music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = GameObject.FindGameObjectWithTag("music");
+
+        Invoke("LoadFirstLevel", 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
