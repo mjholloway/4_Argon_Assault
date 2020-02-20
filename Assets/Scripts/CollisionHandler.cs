14bbebdee4f36f72857e794b04a89c4badfdd290
hollowaymj <michaeljholloway@gmail.com>
2020-02-20 16:12:38 -0500

    Enemies explode, player explodes, refined waypoint tracker

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,16 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // okay as long as this is the only script that loads scenes
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [SerializeField] float LoadLevelDelay = 1f;
+    [SerializeField] GameObject deathFx;
+
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFx.SetActive(true);
+        Invoke("ReloadLevel", LoadLevelDelay);
     }
 
     private void StartDeathSequence()
     {
         gameObject.SendMessage("DetermineDeath");
+    }
+
+    private void ReloadLevel() // string referenced
+    {
+        SceneManager.LoadScene(1);
     }
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFx;
+    [SerializeField] Transform parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject Fx = Instantiate(deathFx, transform.position, Quaternion.identity);
+        Fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
